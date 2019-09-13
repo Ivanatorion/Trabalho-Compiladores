7b996e7c89a88310dc180193fae34623b7ce2d51
mjwille <willejantschmarcelo@gmail.com>
2019-09-13 16:40:45 -0300

    Mais testes

--- testes/run.py
+++ testes/run.py
@@ -7,10 +7,13 @@ def test_compiler(result, error):
         folder = "erro"
     # para todos os arquivos da pasta, aplica os testes
     for file in os.listdir("../testes/{}".format(folder)):
-        test = os.system("./" + sys.argv[1] + " < ../testes/{}/{} > /dev/null ".format(folder, file))
+        test = os.system("./" + sys.argv[1] + " < ../testes/{}/{} > ./log.txt ".format(folder, file))
         if test != result:
             error = True
             print("Erro no teste: {}/{}".format(folder, file))
+            print("Output:")
+            os.system("cat ./log.txt")
+            os.system("rm ./log.txt")
             print("---------------------------------------")
     
     return error
@@ -26,11 +29,13 @@ if __name__ == "__main__":
             print("Erro ao abrir esta pasta.")
         else:
             error = False
-            os.system("make")
+            print("Fazendo make...")
+            os.system("make > /dev/null")
             print("---------------------------------------")
             error = test_compiler(0, error)             # Testes OK
-            error = test_compiler(256, error)            # Testes com Erro
+            error = test_compiler(256, error)           # Testes com Erro
             if not error:
                 print("Tudo OK!")
                 print("---------------------------------------")
-            os.system("make clean")
+            print("Fazendo make clean...")
+            os.system("make clean > /dev/null")
